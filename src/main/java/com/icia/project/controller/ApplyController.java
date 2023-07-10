0797d9f5d4f37c41edb22aa018cc5d46fbb1ed0b
Namyunha <ajs998@naver.com>
2023-07-10 18:26:52 +0900

    모임 신청 인원 처리하기

--- src/main/java/com/icia/project/controller/ApplyController.java
+++ src/main/java/com/icia/project/controller/ApplyController.java
@@ -1,8 +1,11 @@
 package com.icia.project.controller;
 
 
+import com.icia.project.Entity.StudygroupEntity;
 import com.icia.project.dto.ApplyDTO;
+import com.icia.project.dto.StudygroupDTO;
 import com.icia.project.service.ApplyService;
+import com.icia.project.service.StudygroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,29 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/apply")
 public class ApplyController {
-
     private final ApplyService applyService;
+    private final StudygroupService studygroupService;
 
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody ApplyDTO applyDTO) {
         System.out.println("applyDTO = " + applyDTO);
-        applyService.save(applyDTO);
-        return new ResponseEntity<>(HttpStatus.OK);
+        StudygroupDTO studygroupDTO = studygroupService.findById(applyDTO.getPartyId());
+        System.out.println("studygroupDTO.getUserCount() = " + studygroupDTO.getUserCount());
+        if (studygroupDTO.getUserCount() == studygroupDTO.getPartyPersonnel() || studygroupDTO.getUserCount() > studygroupDTO.getPartyPersonnel()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        } else {
+            applyService.save(applyDTO);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
