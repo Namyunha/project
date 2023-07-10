b6ac5536c15b6ca584f71ae75b6edec03d1cc28b
Namyunha <ajs998@naver.com>
2023-07-10 17:50:01 +0900

    모임신청 거절처리

--- src/main/java/com/icia/project/dto/MemberDTO.java
+++ src/main/java/com/icia/project/dto/MemberDTO.java
@@ -4,7 +4,6 @@ import com.icia.project.Entity.MemberEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
